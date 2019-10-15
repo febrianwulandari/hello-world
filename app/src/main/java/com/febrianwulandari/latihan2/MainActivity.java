a60772112da5d783f210d534ff2efe96893d1ec4
Febrian Wulandari <febrian_wulandari_25rpl@student.smktelkom-mlg.sch.id>
2019-10-15 11:39:12 +0700

    1.commit pertama project VSGA 2.skor basket

--- app/src/main/java/com/febrianwulandari/latihan2/MainActivity.java
+++ app/src/main/java/com/febrianwulandari/latihan2/MainActivity.java
@@ -3,12 +3,24 @@ package com.febrianwulandari.latihan2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int skorA = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lain2);
+    }
+
+    public void tambah3PoinA(View view) {
+        skorA = skorA+3;
+        TextView textViewA = findViewById(R.id.skorA);
+        textViewA.setText(String.valueOf(skorA));
+
+
     }
 }
